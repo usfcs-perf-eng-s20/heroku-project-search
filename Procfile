c6f5428cb3bb994d702970606757fe8266af3138
Daniel Havir <havir.daniel@gmail.com>
2020-02-09 23:42:47 -0800

    commit initial files.

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: bin/project-go-search
---
+++ main.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"log"
+	"net/http"
+	"os"
+
+	"github.com/gin-gonic/gin"
+	_ "github.com/heroku/x/hmetrics/onload"
+)
+
+func main() {
+	port := os.Getenv("PORT")
+
+	if port == "" {
+		log.Fatal("$PORT must be set")
+	}
+
+	router := gin.New()
+	router.Use(gin.Logger())
+	router.LoadHTMLGlob("templates/*.tmpl.html")
+
+	router.GET("/", func(c *gin.Context) {
+		c.HTML(http.StatusOK, "index.tmpl.html", nil)
+	})
+
+	router.GET("/helloworld", func(c *gin.Context) {
+		c.HTML(http.StatusOK, "helloworld.tmpl.html", nil)
+	})
+
+	router.Run(":" + port)
+}
